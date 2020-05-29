159953b05af9ce9fe50b11b0fc174307d57e2fc4
ezeli <327578505@qq.com>
2020-05-29 20:28:08 +0800

    fix ppl

--- eval_ppl.py
+++ eval_ppl.py
@@ -6,7 +6,7 @@ import kenlm
 
 sentis = ['positive', 'negative', 'neutral']
 lm_dir = './data/corpus'
-idx2word = './data/captions/idx2word.json'
+idx2word = json.load(open('./data/captions/idx2word.json', 'r'))
 word2idx = {}
 for i, w in enumerate(idx2word):
     word2idx[w] = i
@@ -23,7 +23,8 @@ def compute_ppl(captions_file_prefix):
         lm = lms[sentiments[cap['image_id']]]
         caption = cap['caption']
         caption = caption.split()
-        caption = [word2idx[w] for w in caption]
+        caption = [str(word2idx[w]) for w in caption]
+        caption = ' '.join(caption)
         full_scores = lm.full_scores(caption, bos=True, eos=True)
         lp, _, _ = zip(*full_scores)
         # import pdb
